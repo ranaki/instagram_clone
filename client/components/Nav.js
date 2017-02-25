20345cbb7650de32aa19c61e602467ffdaebab33
Cameron Bullock <bullockcameron@gmail.com>
2017-02-25 16:00:51 -0700

    New nav

--- client/components/Nav.js
+++ client/components/Nav.js
@@ -11,19 +11,23 @@ class Nav extends React.Component {
       <nav>
         <div className="nav-wrapper white">
           <div className="container">
-              <a href="#!" className="brand-logo black-text"><i className="fa fa-instagram fa-fw" aria-hidden="true"></i>Instagram</a>
-              <a href="#" data-activates="mobile" className="button-collapse"><i className="material-icons">menu</i></a>
-            <ul className="right hide-on-med-and-down">
-              <li><a href="/" className="black-text btn blue-grey lighten-4">Post</a></li>
-            </ul>
-            <ul className="side-nav" id="mobile">
-              <li><a href="/">Home</a></li>
-            </ul>
+            <div className="row header-row">
+              <div className="col m4 s12 insta-logo">
+                <img id="logo-icon" src="https://stephencastleberry.files.wordpress.com/2016/05/instagram-logo-black.png?w=620" alt=""/>
+                <img id="wordmark" src="http://vignette3.wikia.nocookie.net/logopedia/images/b/b7/Instagram_2016_wordmark.svg/revision/latest?cb=20160512092125" alt=""/>
+                </div>
+                <input className="col m4 hide-on-small-only" type="text" placeholder="search" />
+                <div className="col m4 hide-on-small-only">
+                  <i className="right fa fa-user-o" aria-hidden="true"></i>
+                  <i className="right fa fa-heart-o" aria-hidden="true"></i>
+                  <i className="right fa fa-compass" aria-hidden="true"></i>
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-      </nav>
-    );
+        </nav>
+      );
+    }
   }
-}
 
-export default Nav;
+  export default Nav;
