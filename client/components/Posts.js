b130b3bb4824edab6eb96ed48f921abbd0471d47
Tyler Foss <tylerscottfoss@gmail.com>
2017-02-25 10:03:58 -0700

    projects update

---
+++ client/components/Posts.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Posts extends React.Component {
+  componentDidMount() {
+    $(".button-collapse").sideNav();
+  }
+
+  render() {
+    return (
+
+    );
+  }
+}
+
+export default App;
