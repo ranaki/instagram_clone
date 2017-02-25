6d4367500aa4b3de7cc7689f81906dbeb6109d9e
rick baker <baker.rickw@gmail.com>
2017-02-25 11:34:43 -0700

    add anothe card

--- client/components/Post.js
+++ client/components/Post.js
@@ -14,11 +14,22 @@ class Post extends React.Component {
         <div className="card">
             <div className="card-image">
               <img src="https://images.unsplash.com/uploads/141219200475673afcb68/f5bd8360?dpr=2&auto=format&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb&crop="></img>
-              <span className="card-title">Card Title</span>
+              <span className="card-title">Surfer</span>
             </div>
             <div className="card-content">
-              <p>I am a very simple card. I am good at containing small bits of information.
-              I am convenient because I require little markup to use effectively.</p>
+              <p>Picture of a surfer.</p>
+            </div>
+            <div className="card-action">
+              <a href="#">This is a link</a>
+            </div>
+          </div>
+          <div className="card">
+            <div className="card-image">
+              <img src="https://images.unsplash.com/uploads/141219324227007364f95/be0967a3?dpr=2&auto=format&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb&crop="></img>
+              <span className="card-title">Camera</span>
+            </div>
+            <div className="card-content">
+              <p>Picture of a camera.</p>
             </div>
             <div className="card-action">
               <a href="#">This is a link</a>
