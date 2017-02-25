6409339f39b4c87c6df49bbb56113eca7b0fc6f9
Jonathan Mancia <jonathanman2010@icloud.com>
2017-02-25 10:32:30 -0700

    Nav changes

--- client/containers/App.js
+++ client/containers/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Nav from '../Nav';
 
 class App extends React.Component {
   componentDidMount() {
@@ -7,18 +8,9 @@ class App extends React.Component {
 
   render() {
     return (
-      <nav>
-        <div className="nav-wrapper">
-          <a href="#!" className="brand-logo">Logo</a>
-          <a href="#" data-activates="mobile" className="button-collapse"><i className="material-icons">menu</i></a>
-          <ul className="right hide-on-med-and-down">
-            <li><a href="/">Home</a></li>
-          </ul>
-          <ul className="side-nav" id="mobile">
-            <li><a href="/">Home</a></li>
-          </ul>
-        </div>
-      </nav>
+      <div>
+        <Nav />
+      </div>
     );
   }
 }
